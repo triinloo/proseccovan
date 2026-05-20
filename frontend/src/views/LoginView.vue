0a6f21eee66da229dfd768bcb5d8f7890a8ccb22
TRIIN <loosaartriin@gmail.com>
2026-05-20 14:13:22 +0300

    Frontend LoginView.vue, AlertError.vue WIP

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,9 +1,19 @@
 <template>
-  <main></main>
+  <div class="container text-center">
+    <div class="col col-4">
+      <h2 class="mb-4">Logi sisse/registreeri</h2>
+      <div class="col col-6">
+        <AlertError :error-message="errorMessage" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import AlertError from '@/components/alerts/AlertError.vue'
+
 export default {
   name: 'LoginView',
+  components: { AlertError },
 }
 </script>
