73c9804db3a3e6b66ea34c2ff35e0d23f8f7a48a
rebanehenry <rebane.henry@gmail.com>
2026-05-20 20:40:21 +0300

    AdminEventFormView task tehtud

--- backend/src/main/java/proseccovan/backend/persistence/bookingpackage/PackageRepository.java
+++ backend/src/main/java/proseccovan/backend/persistence/bookingpackage/PackageRepository.java
@@ -2,5 +2,8 @@ package proseccovan.backend.persistence.bookingpackage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PackageRepository extends JpaRepository<Package, Integer> {
+    Optional<Package> findByName(String name);
 }
