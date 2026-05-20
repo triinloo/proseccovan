c25a9a1703f6eb02524e65877057e5e8920ea59d
Karin <karin.lusikas@gmail.com>
2026-05-20 10:15:05 +0300

    Task Customer Booking View päriselt completed, DELETE ka lisatud

--- backend/src/main/java/proseccovan/backend/service/CustomerBookingService.java
+++ backend/src/main/java/proseccovan/backend/service/CustomerBookingService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import proseccovan.backend.controller.customerbooking.dto.BookingResponseDto;
 import proseccovan.backend.infrastructure.exception.DataNotFoundException;
+import proseccovan.backend.infrastructure.exception.ForbiddenException;
 import proseccovan.backend.persistence.booking.Booking;
 import proseccovan.backend.persistence.booking.BookingRepository;
 import proseccovan.backend.persistence.booking.BookingStatusMapper;
@@ -12,6 +13,7 @@ import proseccovan.backend.persistence.usercontact.UserContactRepository;
 
 import java.time.format.DateTimeFormatter;
 
+import static proseccovan.backend.infrastructure.error.ErrorResponse.CANCELLATION_NOT_ALLOWED;
 import static proseccovan.backend.infrastructure.error.ErrorResponse.DATA_NOT_FOUND;
 
 @Service
@@ -27,20 +29,30 @@ public class CustomerBookingService {
 
         UserContact userContact = userContactRepository.findByUser_Id(booking.getUser().getId());
 
-        BookingResponseDto dto = new BookingResponseDto();
-        dto.setCustomerName(userContact.getUserName());
-        dto.setEmail(booking.getUser().getEmail());
-        dto.setPhoneNumber(userContact.getPhone());
-        dto.setBookingDate(booking.getEventDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-        dto.setBookingType(booking.getBookingTypeInfo());
-        dto.setBookingPackageType(booking.getPackageField().getName());
-        dto.setBookingAddress(booking.getAddress());
-        dto.setLatitude(booking.getLatitude() != null ? booking.getLatitude().toPlainString() : null);
-        dto.setLongitude(booking.getLongitude() != null ? booking.getLongitude().toPlainString() : null);
-        dto.setInfo(booking.getPackageField().getDescription());
-        dto.setBookingStatus(BookingStatusMapper.toBookingStatus(booking.getStatus()));
-        return dto;
+        return new BookingResponseDto(
+                userContact.getUserName(),
+                booking.getUser().getEmail(),
+                userContact.getPhone(),
+                booking.getEventDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                booking.getBookingTypeInfo(),
+                booking.getPackageField().getName(),
+                booking.getAddress(),
+                booking.getLatitude() != null ? booking.getLatitude().toPlainString() : null,
+                booking.getLongitude() != null ? booking.getLongitude().toPlainString() : null,
+                booking.getPackageField().getDescription(),
+                BookingStatusMapper.toBookingStatus(booking.getStatus())
+        );
     }
 
+    public void cancelBooking(Integer bookingId) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new DataNotFoundException(DATA_NOT_FOUND.getMessage(), DATA_NOT_FOUND.getErrorCode()));
 
+        if (!booking.getStatus().equals("O")) {
+            throw new ForbiddenException(CANCELLATION_NOT_ALLOWED.getMessage(), CANCELLATION_NOT_ALLOWED.getErrorCode());
+        }
+
+        booking.setStatus("T");
+        bookingRepository.save(booking);
+    }
 }
