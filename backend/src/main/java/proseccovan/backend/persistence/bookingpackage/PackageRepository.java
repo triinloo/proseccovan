17086b0c332bdddf0e15276ffc62b5ec3a98514d
TRIIN <loosaartriin@gmail.com>
2026-05-21 15:33:33 +0300

    vigade parandus

--- backend/src/main/java/proseccovan/backend/persistence/bookingpackage/PackageRepository.java
+++ backend/src/main/java/proseccovan/backend/persistence/bookingpackage/PackageRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-import java.util.Optional;
-
 public interface PackageRepository extends JpaRepository<Package, Integer> {
 
     @Query("select p from Package p where p.name = :name")
     Optional<Package> findPackageByType(String name);
-    Optional<Package> findByName(String name);
     Optional<Package> findByName(String name);
 }
--- frontend/src/api-services/BookingService.js
+++ frontend/src/api-services/BookingService.js
@@ -1,10 +1,13 @@
-import axios from "axios";
+import axios from 'axios'
 
 
 export default {
   getCustomerBookings(userId) {
     return axios.get(`/api/bookings/user/${userId}`)
+  },
+
   createBooking(data) {
     return axios.post('/api/booking-form', data)
-  },
-}
+    }
+
+  }
