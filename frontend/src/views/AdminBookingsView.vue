0f5c56f0fcc2271b9bb26a2fb343e7915bbebefa
TRIIN <loosaartriin@gmail.com>
2026-05-24 09:35:13 +0300

    Parandatud booking.id kasutamine AdminBookingsView's. Muudetud backend rakenduse nimi ja lisatud package-lock.json.

--- frontend/src/views/AdminBookingsView.vue
+++ frontend/src/views/AdminBookingsView.vue
@@ -26,8 +26,8 @@
         </td>
         <td>
           <span class="badge bg-secondary me-1" style="cursor: pointer" @click="$router.push('/admin-booking/' + booking.id)">Vaata</span>
-          <span v-if="booking.bookingStatus === 'OOTEL'" class="badge bg-success me-1" style="cursor: pointer" @click="confirmBooking(booking.bookingId)">Kinnita</span>
-          <span v-if="booking.bookingStatus === 'OOTEL' || booking.bookingStatus === 'KINNITATUD'" class="badge bg-danger" style="cursor: pointer" @click="cancelBooking(booking.bookingId)">Tühista</span>
+          <span v-if="booking.bookingStatus === 'OOTEL'" class="badge bg-success me-1" style="cursor: pointer" @click="confirmBooking(booking.id)">Kinnita</span>
+          <span v-if="booking.bookingStatus === 'OOTEL' || booking.bookingStatus === 'KINNITATUD'" class="badge bg-danger" style="cursor: pointer" @click="cancelBooking(booking.id)">Tühista</span>
         </td>
       </tr>
       </tbody>
